654de389e9b4a539783bb7c9574c40b3cb48def4
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-05-03 20:09:26 -0300

    Revisao de Usurio e EnderecoUsuario

--- src/Skanboo/src/main/java/com/ti/Skanboo/models/Usuario.java
+++ src/Skanboo/src/main/java/com/ti/Skanboo/models/Usuario.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -31,45 +32,46 @@ public class Usuario {
     @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name="nome", length = 100, nullable=false)
+    @Column(name = "nome", length = 100, nullable = false)
     @NotNull
     @NotEmpty
     @Size(min = 2, max = 100)
     private String nome;
 
-    @Column(name="cpf",length = 11, nullable=false, unique = true)
+    @Column(name = "cpf", length = 11, nullable = false, unique = true)
     @NotNull
     @NotEmpty
     private Integer cpf;
 
-    @Column(name="email", length = 45, nullable=false, unique = true)
+    @Column(name = "email", length = 45, nullable = false, unique = true)
     @NotNull
     @NotEmpty
     private String email;
-    
-    @Column(name="senha", length = 20, nullable=false)
+
+    @Column(name = "senha", length = 20, nullable = false)
     @NotNull
     @NotEmpty
     @Size(min = 6, max = 20)
     private String senha;
 
-    @OneToOne(mappedBy = "usuario")
+    @OneToOne
+    @JoinColumn(name = "id_endereco", referencedColumnName = "id")
     private EnderecoUsuario endereco = new EnderecoUsuario();
 
-    @Column(name="tipo_usuario", nullable=false)
+    @Column(name = "tipo_usuario", nullable = false)
     @NotNull
     @NotEmpty
     private int tipoUsuario;
-     
-    @Column(name="data_nascimento", length = 8, nullable=false)
+
+    @Column(name = "data_nascimento", length = 8, nullable = false)
     @NotNull
     @NotEmpty
     private Integer dataNascimento;
-    
-    @Column(name="foto", length = 255)
+
+    @Column(name = "foto", length = 255)
     private String foto;
-    
-    @Column(name="telefone", length = 11, nullable=false, unique = true)
+
+    @Column(name = "telefone", length = 11, nullable = false, unique = true)
     @NotNull
     @NotEmpty
     private Integer telefone;
