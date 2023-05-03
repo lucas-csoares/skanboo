f262f44eec8071c9957722c2da54f79a11f33944
Lucas Soares <polonio230@hotmail.com>
2023-05-03 17:50:39 -0300

    Adicionando projeto Spring

---
+++ src/Skanboo/src/main/java/com/ti/Skanboo/SkanbooApplication.java
@@ -0,0 +1,13 @@
+package com.ti.Skanboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkanbooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkanbooApplication.class, args);
+	}
+
+}
---
+++ src/Skanboo/src/test/java/com/ti/Skanboo/SkanbooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ti.Skanboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkanbooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
