46a1bd9b6603b3d31ccd1f3bae7f93f7a4a534b3
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-05-04 20:52:01 -0300

    Criacao EnderecoUsuarioController e EnderecoUsuarioService

---
+++ src/Skanboo/src/main/java/com/ti/Skanboo/controllers/EnderecoUsuarioController.java
@@ -0,0 +1,67 @@
+package com.ti.Skanboo.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.ti.Skanboo.models.EnderecoUsuario;
+import com.ti.Skanboo.services.EnderecoUsuarioService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@Validated
+@RequestMapping("/endereco")
+public class EnderecoUsuarioController {
+
+    @Autowired
+    private EnderecoUsuarioService enderecoUsuarioService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EnderecoUsuario> encontrarPorId(@PathVariable Long id) {
+
+        EnderecoUsuario obj = this.enderecoUsuarioService.encontrarPorId(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<EnderecoUsuario> criar(@Valid @RequestBody EnderecoUsuario obj) {
+
+        this.enderecoUsuarioService.criar(obj);
+
+        // Converte o contexto do user para adicionar o path a ele, e transformar em URI
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizar(@Valid @RequestBody EnderecoUsuario obj, @PathVariable Long id) {
+
+        obj.setId(id);
+        obj = this.enderecoUsuarioService.atualizar(obj);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+
+        this.enderecoUsuarioService.deletar(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/Skanboo/src/main/java/com/ti/Skanboo/services/EnderecoUsuarioService.java
@@ -0,0 +1,58 @@
+package com.ti.Skanboo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ti.Skanboo.models.EnderecoUsuario;
+import com.ti.Skanboo.repositories.EnderecoUsuarioRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class EnderecoUsuarioService {
+
+    @Autowired
+    private EnderecoUsuarioRepository enderecoUsuarioRepository;
+
+    public EnderecoUsuario encontrarPorId(Long id) {
+
+        Optional<EnderecoUsuario> endereco = this.enderecoUsuarioRepository.findById(id);
+
+        return endereco.orElseThrow(() -> new RuntimeException("Endereco nao encontrado!"));
+    }
+
+    @Transactional
+    public EnderecoUsuario criar(EnderecoUsuario obj) {
+
+        return this.enderecoUsuarioRepository.save(obj);
+    }
+
+    @Transactional
+    public EnderecoUsuario atualizar(EnderecoUsuario obj) {
+
+        EnderecoUsuario novoEnderecoUsuario = encontrarPorId(obj.getId());
+
+        novoEnderecoUsuario.setBairro(obj.getBairro());
+        novoEnderecoUsuario.setCep(obj.getCep());
+        novoEnderecoUsuario.setCidade(obj.getCidade());
+        novoEnderecoUsuario.setComplemento(obj.getComplemento());
+        novoEnderecoUsuario.setNumero(obj.getNumero());
+        novoEnderecoUsuario.setRua(obj.getRua());
+        novoEnderecoUsuario.setUf(obj.getUf());
+
+        return this.enderecoUsuarioRepository.save(novoEnderecoUsuario);
+    }
+
+    public void deletar(Long id) {
+
+        encontrarPorId(id); // retorna se o endereco existe ou nao
+
+        try {
+            this.enderecoUsuarioRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Nao e possivel excluir o endereco!");
+        }
+    }
+}
