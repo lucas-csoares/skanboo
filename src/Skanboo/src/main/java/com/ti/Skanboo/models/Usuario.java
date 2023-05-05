e0094eca0ae2f7391f17c79d611b4a43b71a1d48
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-05-05 17:08:09 -0300

    Ajuste na classe de Usuario #39

--- src/Skanboo/src/main/java/com/ti/Skanboo/models/Usuario.java
+++ src/Skanboo/src/main/java/com/ti/Skanboo/models/Usuario.java
@@ -56,6 +56,7 @@ public class Usuario {
     @NotNull
     @NotEmpty
     @Size(min = 6, max = 20)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String senha;
 
     @OneToOne(mappedBy = "usuario", cascade = CascadeType.REMOVE)
