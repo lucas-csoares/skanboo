9ba754077a9fa15c1cd209efc1b07c0c20fdb8a2
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-05-07 14:54:00 -0300

    Criado UserSpringSecurity

---
+++ src/Skanboo/src/main/java/com/ti/Skanboo/security/UserSpringSecurity.java
@@ -0,0 +1,56 @@
+package com.ti.Skanboo.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.ti.Skanboo.models.enums.UsuarioEnum;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSpringSecurity(Long id, String username, String password, Set<UsuarioEnum> usuarioEnums) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = usuarioEnums.stream().map(x -> new SimpleGrantedAuthority(x.getDescription()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean hasRole(UsuarioEnum usuarioEnum) {
+        return getAuthorities().contains(new SimpleGrantedAuthority(usuarioEnum.getDescription()));
+    }
+}
