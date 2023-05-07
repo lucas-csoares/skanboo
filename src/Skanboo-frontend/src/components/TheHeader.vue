ba021ea28b98cec367cdc90742442dc3db6ea854
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-05-07 11:09:53 -0300

    Refatoracao do codigo Vue JS #39

--- src/Skanboo-frontend/src/components/TheHeader.vue
+++ src/Skanboo-frontend/src/components/TheHeader.vue
@@ -1,13 +1,9 @@
 <template>
   <header>
     <nav>
-      <router-link to="/login" class="logo"><button>Login</button></router-link>
+      <router-link to="/loginView" class="logo"><button>Login</button></router-link>
 
-      <router-link to="/cadastroinicial" class="logo">
-      <button>Cadastro Inicial</button>
-      </router-link>
-
-      <router-link to="/cadastrofinal" class="logo"><button>Cadastro Final</button></router-link>
+      <router-link to="/cadastroView" class="logo"><button>Cadastrar</button></router-link>
     </nav>
   </header>
 </template>
