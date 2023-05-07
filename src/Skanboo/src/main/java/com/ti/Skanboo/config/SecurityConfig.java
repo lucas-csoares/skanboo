b9ff5d23d2f0e69dcef95b804b47325f7f425b4d
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-05-07 15:24:24 -0300

    Criado JWTUil e UserDetailServiceImpl

--- src/Skanboo/src/main/java/com/ti/Skanboo/config/SecurityConfig.java
+++ src/Skanboo/src/main/java/com/ti/Skanboo/config/SecurityConfig.java
@@ -2,13 +2,17 @@ package com.ti.Skanboo.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -20,6 +24,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     private static final String[] PUBLIC_MATCHERS = { "/" };
 
     private static final String[] PUBLIC_MATCHERS_POST = { "/usuario", "/login" };
@@ -28,6 +37,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.cors().and().csrf().disable();
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder());
+
+        this.authenticationManager = authenticationManagerBuilder.build();
 
         http.authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
---
+++ src/Skanboo/src/main/java/com/ti/Skanboo/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ti.Skanboo.services;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ti.Skanboo.models.Usuario;
+import com.ti.Skanboo.repositories.UsuarioRepository;
+import com.ti.Skanboo.security.UserSpringSecurity;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        
+        Usuario usuario = this.usuarioRepository.findByEmail(email);
+
+        if (Objects.isNull(usuario))
+            throw new UsernameNotFoundException("Usuario nao encontrado " + email);
+        
+
+        return new UserSpringSecurity(usuario.getId(), usuario.getEmail(), usuario.getSenha(), usuario.getPerfil());
+    }
+    
+}
