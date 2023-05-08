f48c6b8606be464df71b52a108c1169ec7b3b43e
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-05-08 16:13:49 -0300

    Criada autenticacao de usuario para cadastro de enderecos #39

--- src/Skanboo/src/main/java/com/ti/Skanboo/services/EnderecoUsuarioService.java
+++ src/Skanboo/src/main/java/com/ti/Skanboo/services/EnderecoUsuarioService.java
@@ -1,13 +1,16 @@
 package com.ti.Skanboo.services;
 
-import java.util.Optional;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ti.Skanboo.exceptions.AuthorizationException;
 import com.ti.Skanboo.models.EnderecoUsuario;
 import com.ti.Skanboo.models.Usuario;
+import com.ti.Skanboo.models.enums.UsuarioEnum;
 import com.ti.Skanboo.repositories.EnderecoUsuarioRepository;
+import com.ti.Skanboo.security.UserSpringSecurity;
 
 import jakarta.transaction.Transactional;
 
@@ -22,21 +25,32 @@ public class EnderecoUsuarioService {
 
     public EnderecoUsuario encontrarPorId(Long id) {
 
-        Optional<EnderecoUsuario> endereco = this.enderecoUsuarioRepository.findById(id);
+        EnderecoUsuario endereco = this.enderecoUsuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Endereco nao encontrado!"));
 
-        return endereco.orElseThrow(() -> new RuntimeException("Endereco nao encontrado!"));
+        // Verifica se usuario esta logado e se o endereco que ele busca pertence a ele
+        UserSpringSecurity userSpringSecurity = UsuarioService.authenticated();
+
+        if (Objects.isNull(userSpringSecurity) || !userSpringSecurity.hasRole(UsuarioEnum.ADMIN)
+                && !EnderecoPertenceAoUsuario(userSpringSecurity, endereco))
+            throw new AuthorizationException("Acesso negado!");
+
+        return endereco;
     }
 
     @Transactional
     public EnderecoUsuario criar(EnderecoUsuario obj) {
+        UserSpringSecurity userSpringSecurity = UsuarioService.authenticated();
+        if (Objects.isNull(userSpringSecurity))
+            throw new AuthorizationException("Acesso negado!");
 
-        Usuario usuario = this.usuarioService.encontrarPorId(obj.getUsuario().getId());
+        Usuario usuario = this.usuarioService.encontrarPorId(userSpringSecurity.getId());
 
         obj.setUsuario(usuario);
 
         return this.enderecoUsuarioRepository.save(obj);
     }
- 
+
     @Transactional
     public EnderecoUsuario atualizar(EnderecoUsuario obj) {
 
@@ -62,5 +76,9 @@ public class EnderecoUsuarioService {
         } catch (Exception e) {
             throw new RuntimeException("Nao e possivel excluir o endereco!");
         }
+    }
+
+    private Boolean EnderecoPertenceAoUsuario(UserSpringSecurity userSpringSecurity, EnderecoUsuario enderecoUsuario) {
+        return enderecoUsuario.getUsuario().getId().equals(userSpringSecurity.getId());
     }
 }
