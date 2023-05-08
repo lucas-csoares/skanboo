c0ef3151bd401ce239ecf9c490af244d39fca8d9
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-05-08 20:36:04 -0300

    Adicionado comentarios no codigo

--- src/Skanboo/src/main/java/com/ti/Skanboo/repositories/UsuarioRepository.java
+++ src/Skanboo/src/main/java/com/ti/Skanboo/repositories/UsuarioRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ti.Skanboo.models.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> /* <Entidade, tipo da variavel Id> */ {
 
     @Transactional(readOnly = true)
     Usuario findByEmail(String email);
-    
+
 }
