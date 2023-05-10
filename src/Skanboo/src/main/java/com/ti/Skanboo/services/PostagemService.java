1dca3c206fe2e92a7e4a42ab7fcdc42fa2d2cf8a
Lucas Soares <polonio230@hotmail.com>
2023-05-10 18:53:48 -0300

    Fazendo o security da postagem

--- src/Skanboo/src/main/java/com/ti/Skanboo/services/PostagemService.java
+++ src/Skanboo/src/main/java/com/ti/Skanboo/services/PostagemService.java
@@ -3,13 +3,19 @@ package com.ti.Skanboo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ti.Skanboo.models.Postagem;
-import com.ti.Skanboo.repositories.PostagemRepository;
-//import com.ti.Skanboo.repositories.UsuarioRepository;
+import com.ti.Skanboo.models.Usuario;
 
+import java.util.Objects;
+import com.ti.Skanboo.repositories.PostagemRepository;
+import com.ti.Skanboo.repositories.UsuarioRepository;
+//import com.ti.Skanboo.repositories.UsuarioRepository;
+import com.ti.Skanboo.models.enums.UsuarioEnum;
+import com.ti.Skanboo.security.UserSpringSecurity;
+import com.ti.Skanboo.exceptions.AuthorizationException;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 @Service
 public class PostagemService {
@@ -20,26 +26,48 @@ public class PostagemService {
     // @Autowired
     // private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     public List<Postagem> listarPostagens() {
         //! revisar se necessario, apos testes
+        UserSpringSecurity userSpringSecurity = UsuarioService.authenticated();
+        if (Objects.isNull(userSpringSecurity))
+            throw new AuthorizationException("Acesso negado!");
 
-        return postagemRepository.findAll();
+        List<Postagem> postagem = this.postagemRepository.findByUsuario_Id(userSpringSecurity.getId());
+        return postagem;
     }
+
+
 
     public Postagem encontrarPorId(Long id) {
         //todo: adicionar metodo de verificacao de userSpringSecurity <- autenticacao de usuario
 
-        Optional<Postagem> postagem = this.postagemRepository.findById(id);
+        Postagem postagem = this.postagemRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Postagem nao encontrada!"));
 
-        return postagem.orElseThrow(() -> new RuntimeException("Postagem nao encontrada!"));
+         // Verifica se usuario esta logado e se o endereco que ele busca pertence a ele
+         UserSpringSecurity userSpringSecurity = UsuarioService.authenticated();
+
+         if (Objects.isNull(userSpringSecurity) || !userSpringSecurity.hasRole(UsuarioEnum.ADMIN)
+                 && !PostagemPertenceAoUsuario(userSpringSecurity, postagem))
+             throw new AuthorizationException("Acesso negado!");
+
+        return postagem;
     }
 
     @Transactional
     public Postagem criar(Postagem obj) {
+        UserSpringSecurity userSpringSecurity = UsuarioService.authenticated();
+        if (Objects.isNull(userSpringSecurity))
+            throw new AuthorizationException("Acesso negado!");
+
+        Usuario usuario = this.usuarioService.encontrarPorId(userSpringSecurity.getId());
         
         obj.setId(null);
 
-        return postagemRepository.save(obj);
+        return this.postagemRepository.save(obj);
     }
 
     @Transactional
@@ -63,4 +91,11 @@ public class PostagemService {
             //? se for generico, sugiro mudar para algo mais generico (ver UsuarioService)
         }
     }
+    
+    private Boolean PostagemPertenceAoUsuario(UserSpringSecurity userSpringSecurity, Postagem postagem) {
+        return postagem.getUsuario().getId().equals(userSpringSecurity.getId());
+    }
+
+
+
 }
