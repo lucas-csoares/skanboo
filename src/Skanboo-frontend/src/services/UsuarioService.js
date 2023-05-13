a4306832c0cd7a88edf50e00ab4cb6c12399a373
giuvanna <gio-vannah@hotmail.com>
2023-05-13 15:33:33 -0300

    redirecionamento refeito

--- src/Skanboo-frontend/src/services/UsuarioService.js
+++ src/Skanboo-frontend/src/services/UsuarioService.js
@@ -1,71 +1,23 @@
-// import { http } from './config';
+import { http } from "./config";
 
 export default {
   criar: (usuario) => {
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve, _reject) => {
-      // não comitar só pra teste
-      resolve({
-        data: `CRIADO ${usuario.nome}`,
-      });
-    });
-    // return { // não comitar só pra teste
-    //   data: "ok"
-    // }
-    // return http.post('usuario', usuario);
+    return http.post("usuario", usuario);
   },
 
   exibirInfo: () => {
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve, _reject) => {
-      resolve({
-        // não comitar só pra teste
-        data: {
-          nome: "Fulano Teste",
-          nascimento: "1999-01-01",
-          email: "teste@dofulano.com",
-          telefone: "999999999",
-          cpf: "99999999999",
-        },
-      });
+    return http.get("usuario", {
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
     });
-    // return http.get('usuario', {
-    //   headers: {
-    //     Authorization: localStorage.getItem('token')
-    //   }
-    // });
   },
 
   atualizar: (usuario) => {
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve, _reject) => {
-      // não comitar só pra teste
-      resolve({
-        data: `ATUALIZADO ${usuario.nome}`,
-      });
-    });
-    // return http.put('usuario', usuario);
+    return http.put("usuario", usuario);
   },
 
   logar: (usuario) => {
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve, _reject) => {
-      // não comitar só pra teste
-      resolve({
-        data: `LOGADO ${usuario.email}`,
-        headers: {
-          getAuthorization: () => {
-            return "tokenSuperSecreto";
-          },
-        },
-      });
-    });
-
-    // return { // não comitar só pra teste
-    //   headers: {
-    //     getAuthorization: "tokenSuperSecreto"
-    //   }
-    // }
-    // return http.post('login', usuario);
+    return http.post("login", usuario);
   },
 };
