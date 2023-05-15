edd9bdedc4decc8c41301d177bd8e65b8888ef09
giuvanna <gio-vannah@hotmail.com>
2023-05-14 22:11:41 -0300

    redirecionamento de login
    - ao logar, usuário é redirecionado para a página do usuário

--- src/Skanboo-frontend/src/services/UsuarioService.js
+++ src/Skanboo-frontend/src/services/UsuarioService.js
@@ -1,23 +1,27 @@
-import { http } from './config';
+import { http } from "./config";
 
 export default {
   criar: (usuario) => {
-    return http.post('usuario', usuario);
+    return http.post("usuario", usuario);
   },
 
   exibirInfo: () => {
-    return http.get('usuario', {
+    return http.get("usuario", {
       headers: {
-        Authorization: localStorage.getItem('token')
-      }
+        Authorization: localStorage.getItem("token"),
+      },
     });
   },
 
   atualizar: (usuario) => {
-    return http.put('usuario', usuario);
+    return http.put("usuario", usuario, {
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    });
   },
 
   logar: (usuario) => {
-    return http.post('login', usuario);
+    return http.post("login", usuario);
   },
 };
