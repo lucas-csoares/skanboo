e0fada97690c550027c249c80032d1289bd80de9
Lucas Soares <polonio230@hotmail.com>
2023-05-25 19:08:44 -0300

    Definindo atributos da classe Troca

---
+++ src/Skanboo/src/main/java/com/ti/Skanboo/repositories/TrocaRepository.java
@@ -0,0 +1,17 @@
+package com.ti.Skanboo.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ti.Skanboo.models.Troca;
+
+@Repository
+public interface TrocaRepository extends JpaRepository<Troca, Long> {
+    
+}
+
+
+
+
+
