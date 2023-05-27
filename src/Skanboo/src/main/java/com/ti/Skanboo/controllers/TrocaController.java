2ccd7f5407854aa5fe91ffc79115cdfe743e0e40
Lucas Soares <polonio230@hotmail.com>
2023-05-27 12:07:41 -0300

    Controller da Troca concluído #63

--- src/Skanboo/src/main/java/com/ti/Skanboo/controllers/TrocaController.java
+++ src/Skanboo/src/main/java/com/ti/Skanboo/controllers/TrocaController.java
@@ -1,18 +1,27 @@
 package com.ti.Skanboo.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ti.Skanboo.models.Oferta;
 import com.ti.Skanboo.models.Troca;
 import com.ti.Skanboo.services.OfertaService;
 import com.ti.Skanboo.services.TrocaService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @Validated
@@ -40,5 +49,35 @@ public class TrocaController {
         return ResponseEntity.ok().body(trocas);
     }
 
+    @PostMapping("/id_oferta")
+    public ResponseEntity<Void> criar(@PathVariable Long id_oferta) {
+        
+        
+        Oferta oferta = this.ofertaService.encontrarPorId(id_oferta);
+        Troca obj = new Troca(oferta);
+
+        this.trocaService.criar(obj);
+        
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable Long id, @Valid @RequestBody Troca obj) {
+        
+        obj.setId(id);
+        obj = this.trocaService.atualizarPorId(obj);
+        return ResponseEntity.noContent().build();
+
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletaPorId(@PathVariable Long id) {
+        this.trocaService.deletarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
     
 }
