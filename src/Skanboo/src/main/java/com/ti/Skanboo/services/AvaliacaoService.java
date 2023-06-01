b81cfd9d54bd6788179cf000e48ab55b6f9b80a0
Lucas Soares <polonio230@hotmail.com>
2023-05-31 23:30:04 -0300

    Tentando implementar função para setar atributo `notaFinal` #64

--- src/Skanboo/src/main/java/com/ti/Skanboo/services/AvaliacaoService.java
+++ src/Skanboo/src/main/java/com/ti/Skanboo/services/AvaliacaoService.java
@@ -1,19 +1,19 @@
 package com.ti.Skanboo.services;
 
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ti.Skanboo.models.Avaliacao;
 import com.ti.Skanboo.models.Troca;
 import com.ti.Skanboo.models.Usuario;
 import com.ti.Skanboo.models.enums.TrocaEnum;
 import com.ti.Skanboo.repositories.AvaliacaoRepository;
 import com.ti.Skanboo.repositories.TrocaRepository;
+import com.ti.Skanboo.repositories.UsuarioRepository;
 import com.ti.Skanboo.security.UserSpringSecurity;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 @Service
 public class AvaliacaoService {
@@ -22,7 +22,7 @@ public class AvaliacaoService {
     private AvaliacaoRepository avaliacao;
 
     @Autowired
-    private TrocaRepository trocaRepository;
+    private UsuarioRepository usuarioRepository;
 
     @Autowired
     private TrocaService trocaService;
@@ -39,9 +39,10 @@ public class AvaliacaoService {
 
 
 
-    //*avaliacao.getTroca().getOferta().getPostagemOrigem().getUsuario().getId() || avaliacao.getTroca().getOferta().getPostagemOfertada().getUsuario().getId()
+   
 
     // Listar avaliações 
+
 
 
     // Criar avaliação
@@ -84,16 +85,47 @@ public class AvaliacaoService {
 
 
 
-    //Atualizar avaliação por Id
+    //Atualizar por id não faz sentido em um contexto da atualização.
 
+    
+    
+    
     
     //Deletar avaliação por id
 
 
 
-    private Boolean trocaPertenceAoUsuario(UserSpringSecurity userSpringSecurity, Avaliacao avaliacao) {
-        return avaliacao.getTroca().getOferta().getPostagemOrigem().getUsuario().getId().equals(userSpringSecurity.getId()) 
-            ||
-            avaliacao.getTroca().getOferta().getPostagemOfertada().getUsuario().getId().equals(userSpringSecurity.getId());
+    
+    
+    
+    
+    
+    private Boolean trocaPertenceAoUsuarioPostagemOrigem(UserSpringSecurity userSpringSecurity, Avaliacao avaliacao) {
+        return avaliacao.getTroca().getOferta().getPostagemOrigem().getUsuario().getId().equals(userSpringSecurity.getId());
     }
+
+    private Boolean trocaPertenceAoUsuarioPostagemOfertada(UserSpringSecurity userSpringSecurity, Avaliacao avaliacao) {
+        return avaliacao.getTroca().getOferta().getPostagemOfertada().getUsuario().getId().equals(userSpringSecurity.getId());
+    }
+    
+
+
+
+
+    public void atualizarNotaFinal(Avaliacao avaliacao) {
+        UserSpringSecurity userSpringSecurity = UsuarioService.authenticated();
+        
+        if (trocaPertenceAoUsuarioPostagemOrigem(userSpringSecurity, avaliacao)
+                || trocaPertenceAoUsuarioPostagemOfertada(userSpringSecurity, avaliacao)) {
+            Usuario usuario = avaliacao.getTroca().getOferta().getPostagemOfertada().getUsuario();
+            List<Avaliacao> avaliacoes = avaliacaoRepository.findByUsuarioAndMes(usuario, LocalDate.now().getMonthValue());
+            int somaNotas = avaliacoes.stream().mapToInt(Avaliacao::getNota).sum();
+            int quantidadeAvaliacoes = avaliacoes.size();
+            double mediaNotas = quantidadeAvaliacoes > 0 ? (double) somaNotas / quantidadeAvaliacoes : 0;
+            usuario.setNotaFinal(mediaNotas);
+            usuarioRepository.save(usuario);
+        }
+    }
+    
+
 }
